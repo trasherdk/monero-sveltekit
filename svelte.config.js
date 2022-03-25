6c14103efaf99dcf222dcd79d35b64d5c4a931a3
TrasherDK <trasherdk@fumlersoft.dk>
2022-03-25 09:47:47 +0100

    Add aliases to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,6 +10,16 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte'
+		vite: {
+			resolve: {
+				alias: {
+					// these are the aliases and paths to them
+					'@components': path.resolve('./src/lib/components'),
+					'@lib': path.resolve('./src/lib'),
+					'@utils': path.resolve('./src/lib/utils')
+				}
+			}
+		}
 	}
 };
 
