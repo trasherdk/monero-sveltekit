ac6e0df77322ac3b4a2036a15932c37c15d22329
TrasherDK <trasherdk@fumlersoft.dk>
2022-03-25 09:48:37 +0100

    Add navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import {
+		Collapse,
+		Navbar,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavItem,
+		NavLink,
+		Icon
+	} from 'sveltestrap';
+
+	// variable that control whether mobile menu is open or closed
+	let isOpen = false;
+	let isLoggedIn = false;
+
+	const toggle = () => (isOpen = !isOpen);
+
+	const handleUpdate = () => {
+		return {};
+	};
+</script>
+
+<Navbar color="light" light expand="md">
+	<NavbarBrand href="/">Monero Sveltekit</NavbarBrand>
+	<NavbarToggler on:click={toggle} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ms-auto" navbar>
+			<NavItem>
+				<NavLink href="/about">About</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink href="/login"><Icon name="key-fill" style="color:red" /> Login</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink>Logout</NavLink>
+			</NavItem>
+		</Nav>
+	</Collapse>
+</Navbar>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { Styles } from 'sveltestrap';
+	import Navbar from '@components/navbar.svelte';
 </script>
 
+<Navbar />
 <slot />
 <Styles />
