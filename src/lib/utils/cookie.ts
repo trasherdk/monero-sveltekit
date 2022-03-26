fd4f02c04979699deac3fce96ba495e5e05df44d
TrasherDK <trasherdk@fumlersoft.dk>
2022-03-26 14:53:19 +0100

    fix: param default to empty

--- src/lib/utils/cookie.ts
+++ src/lib/utils/cookie.ts
@@ -1,5 +1,7 @@
-export const parseCookie = (str) => {
+export const parseCookie = (str = '') => {
   const result = {}
+  if (str === '') return result
+
   for (const cookie of str.split(';')) {
     const [name, value] = cookie.trim().split('=')
     result[name] = value
