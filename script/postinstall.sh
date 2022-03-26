b45d290379f45cc0d3639bb64b50999575d68591
TrasherDK <trasherdk@fumlersoft.dk>
2022-03-27 00:03:14 +0100

    pnpm paths suck

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -2,15 +2,22 @@
 
 BASE="$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")"
 cd "${BASE}" || exit 1
-LIB="${BASE}/node_modules/monero-javascript"
-DST="${BASE}/static/monero-javascript"
+
+VER=$(grep 'monero-javascript' package.json | cut -f4 -d'"' | cut -f2 -d'^')
+if [ -z ${VER} ]; then
+  echo "${WHITE}* monero-javascript ${RED} is not installed.${RESTORE}"
+  exit
+fi
+
+LIB="node_modules/monero-javascript"
+DST="static/monero-javascript"
 
 #if [ -f "${LIB}/dist/monero_wallet_full.js" ]; then
-if [ -f node_modules/monero-javascript/dist/monero_wallet_full.js ]; then
+if [ -f ${LIB}/dist/monero_wallet_full.js ]; then
   if [ ! -d "${DST}" ]; then
     mkdir -p ${DST}
   fi
-  cp -r "${LIB}/dist/*" "${DST}/"
+  cp "${LIB}/dist"/* "${DST}/"
   cp "${LIB}/src/main/js/common/MoneroWebWorker.js" "${DST}/"
 else
   echo "${RED}* Monero Javascript not found: ${LIB}${RESTORE}"
