ffd588c52ad91f63342db6bb609d8d143a253685
TrasherDK <trasherdk@fumlersoft.dk>
2022-04-06 19:30:06 +0200

    TickerChart: calculate inner width

---
+++ src/lib/components/TickerChart.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+</script>
+
+<script>
+	/* Calculate the inner width of the drawing area */
+	const chartEL = document.getElementById('chart');
+	const chartWidth = chartEL.clientWidth;
+	const borderWidth = parseInt(chartEL.style.borderWidth.valueOf()) * 2;
+	const paddingWidth =
+		parseInt(window.getComputedStyle(chartEL, null).getPropertyValue('padding-left')) * 2;
+	const svgWidth = chartWidth - (borderWidth + paddingWidth);
+</script>
